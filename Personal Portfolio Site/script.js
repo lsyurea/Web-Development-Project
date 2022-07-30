c8a294f8539ad588cba6f12c897862b64587c7e7
Lee Si Yuan <lee.si.yuan.20@gmail.com>
2022-07-31 01:29:31 +0800

    added ability to create timeline

--- Personal Portfolio Site/script.js
+++ Personal Portfolio Site/script.js
@@ -1,5 +1,22 @@
 const timeline = document.querySelector('.timeline');
 
-function createTimeLine() {
-    PaymentResponse;
+function createTimeLine(...args) {
+    let choice = args;
+    let n = choice.length;
+    const mainLine = document.createElement('div');
+    mainLine.className = 'mainline';
+    for (let i = 0; i < n; i++){
+        let sideLine = document.createElement('div');
+        let text = document.createElement('div');
+        sideLine.className = 'sideline';
+        sideLine.style.setProperty('--position', `${(100/(n + 1))*(i + 1)}%`);
+        text.style.setProperty('--position', `${(100/(n + 1))*(i + 1)}%`);
+        text.className = 'text';
+        text.innerText = choice[i];
+        
+        mainLine.append(sideLine, text);
+    }
+    timeline.append(mainLine);
 }
+
+createTimeLine('Education', 'Experience', 'Hobbies');
